4bd06baeed3103560b3a1b349e4338499dfb2878
Rye <stanley05ryuu@gmail.com>
2024-06-20 01:15:57 +0700

    THE CHANGE THE CHANGE IS REALLLLLLLLLLLLLLLLLLLLLLLLLLLLLL

--- Javascript/directMessage.js
+++ Javascript/directMessage.js
@@ -1,10 +1,11 @@
 
 async function fetchOutput() {
+    var userInput = document.getElementById("user-input").value;
 
     var chatMessages = document.getElementById("chat-messages");
     var sellerMessage = document.createElement("div");
     sellerMessage.className = "message -message";
-    sellerMessage.innerHTML = "<span class='user'>Seller: <br></span> <span class='text'>" + "Lu asik bang" + "</span>";
+    sellerMessage.innerHTML = "<span class='user'>User: <br></span> <span class='text'>" + userInput + "</span>";
     chatMessages.appendChild(sellerMessage);
 
     chatMessages.scrollTop = chatMessages.scrollHeight;
